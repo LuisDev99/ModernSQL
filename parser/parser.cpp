4c788d56507320ce63ef6c9b334c8285e5700bf4
Luis Gerardo <luisgpr99@gmail.com>
2020-01-30 09:39:08 -0600

    Added grammar function and also implemented the select query

--- parser/parser.cpp
+++ parser/parser.cpp
@@ -1,11 +1,51 @@
 #include "parser.h"
 
-void Parser::action()
+#define GET_NEXT_TOKEN() \
+    lookAhead = getNextToken();
+
+#define GET_TOKEN_STRING_AND_GET_NEXT_TOKEN()     \
+    std::string moveToken()                       \
+    {                                             \
+        std::string text = this->lexer->YYTEXT(); \
+        GET_NEXT_TOKEN();                         \
+        return text;                              \
+    }
+
+void Parser::statement()
 {
+    if (this->lookAhead == Token::Select)
+        select();
+
+    else if (this->lookAhead == Token::Update)
+        update();
+
+    else if (this->lookAhead == Token::Delete)
+        Delete();
+
+    else if (this->lookAhead == Token::Create || this->lookAhead == Token::Insert)
+        insert();
+
+    else
+        throw UnexpectedPointReachedException(__func__, __FILE__, __LINE__);
 }
 
 void Parser::select()
 {
+    /* SELECT */
+    moveIfLookAheadEqualsTo(Token::Select);
+
+    /* OPEN_BRACE */
+    moveIfLookAheadEqualsTo(Token::Open_Brace);
+
+    /* obj-body-decl */
+    obj_body_decl();
+
+    /* (condition-body)? */
+    if (lookAheadEqualsTo(Token::Where))
+        condition_body();
+
+    /* CLOSE_BRACE */
+    moveIfLookAheadEqualsTo(Token::Close_Brace);
 }
 void Parser::update()
 {
@@ -13,27 +53,207 @@ void Parser::update()
 void Parser::Delete()
 {
 }
-void Parser::create()
+void Parser::insert()
 {
 }
 void Parser::obj_body_decl()
 {
+    /* IDENTIFIER */
+    moveIfLookAheadEqualsTo(Token::Identifier);
+
+    /* (body)? */
+    if (lookAheadEqualsTo(Token::Open_Brace))
+        body();
 }
+
+void Parser::body()
+{
+    /* OPEN_BRACE */
+    moveIfLookAheadEqualsTo(Token::Open_Brace);
+
+    /* attribute */
+    attribute();
+
+    /* (attribute)* */
+    while (lookAheadEqualsTo(Token::Identifier))
+        attribute();
+
+    /* CLOSE_BRACE */
+    moveIfLookAheadEqualsTo(Token::Close_Brace);
+}
+
 void Parser::attribute()
 {
+    /* IDENTIFIER */
+    moveIfLookAheadEqualsTo(Token::Identifier);
+
+    /* attribute-prime */
+    attribute_prime();
 }
+
+void Parser::attribute_prime()
+{
+    if (lookAheadEqualsTo(Token::Open_Par))
+    {
+        /* OPEN_PAR */
+        GET_NEXT_TOKEN();
+
+        /* IDENTIFIER */
+        moveIfLookAheadEqualsTo(Token::Identifier);
+
+        /* CLOSE_PAR */
+        moveIfLookAheadEqualsTo(Token::Close_Par);
+
+        return;
+    }
+
+    if (lookAheadEqualsTo(Token::Identifier))
+    {
+        /* IDENTIFIER */
+        GET_NEXT_TOKEN();
+
+        /* USIGN | ON */
+        if (lookAheadEqualsTo(Token::Using) || lookAheadEqualsTo(Token::On))
+        {
+            GET_NEXT_TOKEN();
+        }
+        else
+        {
+            throw "Expected Token Using or On!";
+        }
+
+        /* foreign-key */
+        foreign_key();
+    }
+
+    /* epsilon */
+    return;
+}
+
+void Parser::foreign_key()
+{
+    /* IDENTIFIER */
+    moveIfLookAheadEqualsTo(Token::Identifier);
+
+    /* (body)? */
+    if (lookAheadEqualsTo(Token::Open_Brace))
+        body();
+}
+
 void Parser::condition_body()
 {
+    /* WHERE */
+    moveIfLookAheadEqualsTo(Token::Where);
+
+    /* OPEN_BRACE */
+    moveIfLookAheadEqualsTo(Token::Open_Brace);
+
+    /* conditional-obj-body-decl */
+    conditional_obj_body_decl();
+
+    /* CLOSE_BRACE */
+    moveIfLookAheadEqualsTo(Token::Close_Brace);
 }
 void Parser::conditional_obj_body_decl()
 {
+    /* expression */
+    expression();
+
+    /* (expression)* */
+    while (isExpr(this->lookAhead))
+        expression();
 }
 void Parser::expression()
 {
+    /* foo */
+    foo();
+
+    while (isThirdLevelOp(this->lookAhead))
+    {
+        GET_NEXT_TOKEN();
+
+        expression();
+    }
 }
+
 void Parser::foo()
 {
+    /* bar */
+    bar();
+
+    while (isSecondLevelOp(this->lookAhead))
+    {
+        GET_NEXT_TOKEN();
+
+        foo();
+    }
 }
+
 void Parser::bar()
 {
+    /* IDENTIFIER */
+    if (lookAheadEqualsTo(Token::Identifier))
+    {
+        GET_NEXT_TOKEN();
+
+        /* bar-prime */
+        bar_prime();
+
+        return;
+    }
+
+    /* BOOL_LITERAL */
+    if (lookAheadEqualsTo(Token::Bool_Literal))
+    {
+        GET_NEXT_TOKEN();
+
+        return;
+    }
+
+    /* INT_LITERAL */
+    if (lookAheadEqualsTo(Token::Int_Literal))
+    {
+        GET_NEXT_TOKEN();
+
+        return;
+    }
+
+    /* STRING_LITERAL */
+    if (lookAheadEqualsTo(Token::String_Literal))
+    {
+        GET_NEXT_TOKEN();
+
+        return;
+    }
+
+    /* LOGICAL_NOT_OPERATOR */
+    if (lookAheadEqualsTo(Token::Logical_Not_Operator))
+    {
+        GET_NEXT_TOKEN();
+
+        /* expression */
+        expression();
+
+        return;
+    }
+
+    throw UnexpectedPointReachedException(__func__, __FILE__, __LINE__);
+}
+
+void Parser::bar_prime()
+{
+    /* epsilon */
+    if (!lookAheadEqualsTo(Token::Open_Brace))
+        return;
+
+    /* --- Else --- */
+
+    /* OPEN_BRACE */
+    GET_NEXT_TOKEN();
+
+    /* conditional-obj-body-decl */
+    conditional_obj_body_decl();
+
+    /* CLOSE_BRACE */
+    moveIfLookAheadEqualsTo(Token::Close_Brace);
 }
--- parser/parser.h
+++ parser/parser.h
@@ -3,24 +3,46 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <FlexLexer.h>
 #include "./../tokens.h"
+#include "./../exceptions.cpp"
 
 class Parser
 {
 
 public:
+    /* Grammar productions in order */
+    void statement();
+    void select();
+    void update();
+    void Delete();
+    void insert();
+    void obj_body_decl();
+    void body();
+    void attribute();
+    void attribute_prime();
+    void foreign_key();
+    void condition_body();
+    void conditional_obj_body_decl();
+    void expression();
+    void foo();
+    void bar();
+    void bar_prime();
+
+public:
+    Parser() = default;
+
     Parser(std::istream &input) : in(input)
     {
         lexer = new yyFlexLexer(&in);
 
-        lookAhead = (Token)lexer->yylex();
+        lookAhead = getNextToken();
+    }
 
-        do
-        {
-            std::cout << "Token: " << lexer->YYText() << " token value: " << (int)lookAhead << '\n';
-
-        } while (lookAhead = (Token)lexer->yylex());
+    void beginParsing()
+    {
+        statement();
     }
 
     ~Parser()
@@ -28,20 +50,70 @@ public:
         delete lexer;
     }
 
-private:
-    /* Grammar productions in order */
-    void action();
-    void select();
-    void update();
-    void Delete();
-    void create();
-    void obj_body_decl();
-    void attribute();
-    void condition_body();
-    void conditional_obj_body_decl();
-    void expression();
-    void foo();
-    void bar();
+    Token getNextToken()
+    {
+        return (Token)lexer->yylex();
+    }
+
+    bool lookAheadEqualsTo(Token tk)
+    {
+        return tk == this->lookAhead;
+    }
+
+    /**
+     * Function that checks if lookahead is equal to the @param tk 
+     * If they are not equal, throws an exception
+     * If equal, gets the next token and assigns it to lookahead */
+    void moveIfLookAheadEqualsTo(Token tk)
+    {
+        if (tk != this->lookAhead)
+            throw IllegalTokenException(tk, this->lookAhead);
+
+        lookAhead = getNextToken();
+    }
+
+    bool isExpr(Token tk)
+    {
+        switch (tk)
+        {
+        case Token::Identifier:
+        case Token::Bool_Literal:
+        case Token::Int_Literal:
+        case Token::String_Literal:
+        case Token::Logical_Not_Operator:
+            return true;
+        }
+
+        return false;
+    }
+
+    bool isThirdLevelOp(Token tk)
+    {
+        switch (tk)
+        {
+        case Token::Logical_LessEqual_Operator:
+        case Token::Logical_GreaterEqual_Operator:
+        case Token::Logical_LessThan_Operator:
+        case Token::Logical_GreaterThan_Operator:
+        case Token::Logical_Equal_Operator:
+        case Token::Logical_NotEqual_Operator:
+            return true;
+        }
+
+        return false;
+    }
+
+    bool isSecondLevelOp(Token tk)
+    {
+        switch (tk)
+        {
+        case Token::Logical_And_Operator:
+        case Token::Logical_Or_Operator:
+            return true;
+        }
+
+        return false;
+    }
 
 private:
     std::istream &in;
